c72a2fdd1370651e7574a678461f3a4c09e4284b
Julian Weimer <julian.weimer@gmail.com>
2013-03-06 20:32:38 +0100

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,32 @@
+$(function() {
+
+    $('#map, #slides').remove().insertAfter('section#body_content h1');
+
+    $('#map').goMap({
+        maptype: 'ROADMAP',
+        latitude: 53.56423,
+        longitude: 9.98309,
+        zoom: 15,
+        markers: [{latitude: 53.56423, longitude: 9.98309, title: 'Mini-Hotel an der Messe'}]
+    });
+
+    $('#slides').slides({
+        generateNextPrev: true,
+        generatePagination: false,
+        effect: 'fade',
+        play: 5000
+    });
+
+    $('a.booking_button').fancybox({
+      onStart: function(){
+                 $('div#hotelspider').show();
+               },
+      onComplete: function(){
+                 $('div#hotelspider iframe').attr('src','https://reservation.tourisoft.ch/hotel-spider/hotel/index.php?HotelCode=1554');
+               },
+      onClosed: function(){
+                 $('div#hotelspider').hide();
+                }
+    });
+
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniHotel::Application.config.secret_token = '01ef35c5293a728c4a6dafc745011bb3e5259c8e1e055e1db71146e569dbcdac566d325055432124640aae6b19164bd22b63013b11293fbe08665752ed3abed7'
